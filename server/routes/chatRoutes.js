844bc8f30f5dafd242094994a607a9ddfd2d4b2f
Do Thang <110244660+tuanthu1@users.noreply.github.com>
2026-08-17 15:19:20 +0700

    Delete server directory

--- server/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-const {verifyToken} = require('../middleware/auth');
-router.post('/', verifyToken, chatController.processChat);
-router.post('/admin', verifyToken, chatController.processAdminChat);
-
-module.exports = router;
